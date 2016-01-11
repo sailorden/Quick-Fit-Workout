4929951a5e75378d9e1dca72319ca9893d9c997e
Todd Kronenberg <toddkronenberg@gmail.com>
2016-01-10 19:36:44 -0600

    Basic blank ionic app with my project structure.

---
+++ src/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('quickfit', ['ionic'])
+
+.run(($ionicPlatform) => {
+  $ionicPlatform.ready(() => {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
