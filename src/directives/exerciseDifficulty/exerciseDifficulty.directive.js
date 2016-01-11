2d12f9475339b5a7421d0cadeffcb3afd80a9ecd
Todd Kronenberg <toddkronenberg@gmail.com>
2016-01-11 15:51:53 -0600

    Styled the exercise difficulty directive.

--- src/directives/exerciseDifficulty/exerciseDifficulty.directive.js
+++ src/directives/exerciseDifficulty/exerciseDifficulty.directive.js
@@ -8,6 +8,9 @@ function qfExerciseDifficulty() {
   return {
     restrict: 'E',
     scope: {},
+    link: (scope, elem, attrs) => {
+      scope.max = 25;
+    },
     templateUrl: 'directives/exerciseDifficulty/exerciseDifficulty.template.html'
   };
 }
