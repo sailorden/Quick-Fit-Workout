f4211fd4be4f503164fcc5bd142f2fcf7c72486a
Todd Kronenberg <toddkronenberg@gmail.com>
2016-01-12 13:34:13 -0600

    Made back button directive, put in two views.

--- src/directives/exercise/exercise.directive.js
+++ src/directives/exercise/exercise.directive.js
@@ -4,13 +4,15 @@ angular.module('quickfit')
 
 .directive('qfExercise', qfExercise);
 
-qfExercise.$inject = ['$stateParams'];
-function qfExercise($stateParams) {
+qfExercise.$inject = ['$stateParams', '$state'];
+function qfExercise($stateParams, $state) {
   return {
     restrict: 'E',
     scope: {},
     link: (scope, elem, attrs) => {
       scope.exercise = $stateParams.exercise;
+      scope.$on('$ionicView.enter', (e) =>
+        scope.back = $state.current.name === 'exercise.difficulty');
     },
     templateUrl: 'directives/exercise/exercise.template.html'
   };
--- src/directives/workout/workout.directive.js
+++ src/directives/workout/workout.directive.js
@@ -4,13 +4,15 @@ angular.module('quickfit')
 
 .directive('qfWorkout', qfWorkout);
 
-qfWorkout.$inject = ['$stateParams'];
-function qfWorkout($stateParams) {
+qfWorkout.$inject = ['$stateParams', '$state'];
+function qfWorkout($stateParams, $state) {
   return {
     restrict: 'E',
     scope: {},
     link: (scope, elem, attrs) => {
       scope.workout = $stateParams.workout;
+      scope.$on('$ionicView.enter', (e) =>
+        scope.back = $state.current.name === 'workout.start');
     },
     templateUrl: 'directives/workout/workout.template.html'
   };
