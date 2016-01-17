a60a41e940ce1eff6ec1886620a66e0d8a22f6a9
Todd Kronenberg <toddkronenberg@gmail.com>
2016-01-17 17:06:21 -0600

    Add audio files, gave timer 5 sec audio countdown.

--- src/directives/shared/timer/timer.directive.js
+++ src/directives/shared/timer/timer.directive.js
@@ -12,12 +12,25 @@ function qfTimer($interval, $timeout) {
     link: (scope, elem, attrs) => {
       scope.tick = scope.workout.time;
       let intervalTimer;
+      const startSound = 5;
+      let beep = new Audio('../../../audio/beep.wav');
+      beep.loop = false;
+      let endBeep = new Audio('../../../audio/timesUp.wav');
+      endBeep.loop = false;
+      let soundSecond = startSound;
       startCountdown();
 
       function startCountdown() {
         let start = new Date().getTime();
         let elapsedSecs = 0, elapsedMillis = 0;
         intervalTimer = $interval(() => {
+          if (scope.tick <= startSound && scope.tick !== 0 && soundSecond === scope.tick) {
+            beep.play();
+            soundSecond--;
+          } else if(scope.tick === 0 && soundSecond === scope.tick) {
+            endBeep.play();
+            soundSecond--;
+          }
           if (scope.tick === 0) timeUp();
           else {
             elapsedMillis = new Date().getTime() - start;
